81320381c49f629fa25e8037b4bdb37ef154f6e9
MiloHakai <81438550+MiloHakai@users.noreply.github.com>
2023-07-22 16:49:50 +0200

    Update hug.py

--- hug/hug.py
+++ hug/hug.py
@@ -14,6 +14,8 @@ class hug(commands.Cog):
             "https://media1.giphy.com/media/lrr9rHuoJOE0w/giphy.gif?cid=ecf05e47pwt7i8zqysdj0q3qzv41n2b6pc4uphk4du21mf4i&rid=giphy.gif",
             "https://media1.giphy.com/media/du8yT5dStTeMg/giphy.gif?cid=ecf05e47l52y8u7meoxxkd0iunvkn87zbf6mudc7h0k6zwu4&rid=giphy.gif"
         ]
+        self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
+        self.config.register_guild(hug_counter=0)
 
     @commands.command()
     async def hug(self, ctx, member: discord.Member):
@@ -21,16 +23,16 @@ class hug(commands.Cog):
         server_name = ctx.guild.name
         gif_url = random.choice(self.gifs)
         
+        hug_count = await self.config.guild(ctx.guild).hug_counter()
+        await self.config.guild(ctx.guild).hug_counter.set(hug_count + 1)
+        
         embed = discord.Embed(
             title=f"{ctx.author.name} hugged {member.name} in {server_name}!",
             color=discord.Color.purple()
         )
         embed.set_image(url=gif_url)
-
-        if ctx.guild.id not in self.hug_counter:
-            self.hug_counter[ctx.guild.id] = 0
-        self.hug_counter[ctx.guild.id] += 1
-        embed.set_footer(text=f"Hugs given in {server_name}: {self.hug_counter[ctx.guild.id]}")
+        embed.set_footer(text=f"Hugs given in {server_name}: {hug_count + 1}")
+        
         await ctx.send(embed=embed)
 
 def setup(bot):
