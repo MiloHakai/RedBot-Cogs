96c8817cf0b1a031613980d03f952abaf0efae77
MiloHakai <81438550+MiloHakai@users.noreply.github.com>
2023-07-28 14:32:28 +0200

    I am a skill issue

--- kiss/kiss.py
+++ kiss/kiss.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 from redbot.core import Config
 
-class Kiss(commands.Cog):
+class kiss(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.gifs = [
