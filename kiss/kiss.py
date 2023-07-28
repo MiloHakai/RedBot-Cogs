5bc857ca70a579914a54e249db9e4a20ef25ba1d
MiloHakai <81438550+MiloHakai@users.noreply.github.com>
2023-07-28 14:38:11 +0200

    Failed to import lib

--- kiss/kiss.py
+++ kiss/kiss.py
@@ -1,7 +1,6 @@
 import discord
-from discord.ext import commands
+from redbot.core import commands, Config
 import random
-from redbot.core import Config
 
 class kiss(commands.Cog):
     def __init__(self, bot):
